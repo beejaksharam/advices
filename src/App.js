1be380b9a1013ec8634cbb23156ee50051a62ad7
Sureshkumar Munuswamy <13384757+beejaksharam@users.noreply.github.com>
2023-06-10 12:57:49 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+export default function App() {
+  const [advice, setAdvice] = useState("Click to read one...");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
+  }
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Read Advice</button>
+      <p> So far you read {count} advices </p>
+    </div>
+  );
+}
